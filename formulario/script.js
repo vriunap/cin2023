25f4a94f9a03510e7192f93e558ad2076f28fbc8
Johandenis <johandenisaguilarjdar2002@gmail.com>
2023-09-13 07:34:30 -0500

    formulario

---
+++ formulario/script.js
@@ -0,0 +1,14 @@
+function uploadFile(inputId) {
+    document.getElementById(inputId).click();
+}
+
+function showSection(num) {
+    if (num == 2) {
+        document.getElementById('section1').style.display = "none";
+        document.getElementById('section2').style.display = "block";
+    }
+}
+
+function uploadFile(elementId) {
+    document.getElementById(elementId).click();
+}
