8b3f9ba001dcff8ff7c3a0f3aa23bc89953e5e35
Yefer Andersson Mamani Chambi <yefer.andersson.una@gmail.com>
2023-10-19 12:31:09 -0500

    logo

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,8 +4,8 @@ import Nav from "react-bootstrap/Nav";
 import { ThemeContext } from "../../Context/theme";
 
 import Container from "react-bootstrap/Container";
-import logoLight from "../../Assets/logoLight.png";
-import logoDark from "../../Assets/logoDark.png";
+import logoLight from "../../Assets/logoCongreso.png"; //light
+import logoDark from "../../Assets/logoCongreso.png"; //dark
 
 import { Link } from "react-router-dom";
 import "./navbar.css";
